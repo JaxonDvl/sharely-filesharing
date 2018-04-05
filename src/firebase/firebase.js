94545c140761bac564c2017dafbfc92859845025
JaxonDvl <tatualindvl@gmail.com>
2018-04-05 12:45:40 +0300

    routing with context provide

---
+++ src/firebase/firebase.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyAXoBgUNmSCYPZjVIk0n2jzxtIywXu4FDk",
+    authDomain: "sharely-filesharing.firebaseapp.com",
+    databaseURL: "https://sharely-filesharing.firebaseio.com",
+    projectId: "sharely-filesharing",
+    storageBucket: "sharely-filesharing.appspot.com",
+    messagingSenderId: "440466367768"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+const auth = firebase.auth();
+
+export {
+  auth,
+};
